3003bc6ccedd3854524b91a4224e29647a453c86
Tanner Griffin <tannermgriffin@gmail.com>
2020-06-28 12:19:46 -0500

    added class icons to header

--- gh-pt/src/components/Header.js
+++ gh-pt/src/components/Header.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import xp from '../xpicon.svg';
-import wind from '../wind.png';
+import icon1 from '../assets/class-icons/01 Brute.png';
+import icon2 from '../assets/class-icons/04 Scoundrel.png';
+import icon3 from '../assets/class-icons/07 Sun.png';
+import icon4 from '../assets/class-icons/03 Spellweaver.png';
 import '../style/Header.css';
 import ghfont from '../style/PirataOne-Gloomhaven.ttf';
 
@@ -8,15 +11,15 @@ export default function Header() {
     return (
         <header className="header">
             <div className="dimmer">
-                <img src={xp} className="header-class-logo"/>
-                <img src={xp} className="header-class-logo"/>
+                <img src={icon1} className="header-class-logo"/>
+                <img src={icon2} className="header-class-logo"/>
                 <div className="logo">
                     <h1 style={{fontFamily: ghfont}}>Party</h1>
                     <img src={xp} className="App-logo" alt="xp" />
                     <h1>Tracker</h1>
                 </div>
-                <img src={xp} className="header-class-logo"/>
-                <img src={xp} className="header-class-logo"/>
+                <img src={icon3} className="header-class-logo"/>
+                <img src={icon4} className="header-class-logo"/>
             </div>
         </header>
     )
--- gh-pt/src/global.js
+++ gh-pt/src/global.js
@@ -4,6 +4,7 @@ export const GlobalStyles = createGlobalStyle`
   html, body {
     margin: 0;
     padding: 0;
+    height: 100vh;
   }
   *, *::after, *::before {
     box-sizing: border-box;
@@ -14,27 +15,33 @@ export const GlobalStyles = createGlobalStyle`
     justify-content: center;
     align-items: center;
     background: url(https://i.pinimg.com/originals/6b/72/23/6b7223d0def9fb19401c5758808f4178.jpg) no-repeat fixed center;
+    background-size: cover;
     color: ${({ theme }) => theme.primaryLight};
     height: 100vh;
     text-rendering: optimizeLegibility;
   }
   header {
     background: url(./assets/woodgrain.jpg);
-  background-repeat: none;
-  height: 70px;
-  color: whitesmoke;
-  letter-spacing: 6px;
-  text-shadow: 0 5px 14px black;
-  box-shadow: 0 1px 5px black;
-  -webkit-font-smoothing: antialiased;
--moz-osx-font-smoothing: grayscale;
--webkit-text-stroke: .1px rgb(80, 80, 80);
+    background-repeat: no-repeat;
+    background-size: 100% 100%;
+    height: 70px;
+    width: 100vw;
+    z-index: 100;
+    position: absolute;
+    top: 0;
+    color: whitesmoke;
+    letter-spacing: 6px;
+    text-shadow: 0 5px 14px black;
+    box-shadow: 0 3px 18px black;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-text-stroke: .1px rgb(80, 80, 80);
   }
 
   .dimmer {
     display: flex;
-  justify-content: space-around;
-  align-items: center;
+    justify-content: space-around;
+    align-items: center;
     flex: 1;
     background-color: rgba(240, 240, 240, .3);
   }
@@ -42,23 +49,25 @@ export const GlobalStyles = createGlobalStyle`
   .logo {
     // margin-top: 20px;
     height: 70px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  align-self: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    align-self: center;
 }
 
-.header-class-logo {
-  width: 8vh;
-  filter: opacity(50%)
-}
-  
-.App-logo {
-  width: 8vh;
-  pointer-events: none;
-  filter: invert(100%) sepia(100%) saturate(1822%) hue-rotate(290deg)
-    brightness(80%) contrast(119%);
-}
+  .header-class-logo {
+    height: 5vh;
+    max-height: 50px;
+    filter: opacity(65%)
+  }
+    
+  .App-logo {
+    width: 7vh;
+    pointer-events: none;
+    max-height: 50px;
+    filter: invert(100%) sepia(100%) saturate(2022%) hue-rotate(287deg)
+      brightness(72%) contrast(189%);
+  }
   
   @media (prefers-reduced-motion: no-preference) {
     .App-logo {
@@ -80,11 +89,11 @@ export const GlobalStyles = createGlobalStyle`
     text-align: center;
     text-transform: uppercase;
   }
-  img {
-    border-radius: 5px;
-    height: auto;
-    width: 10rem;
-  }
+  // img {
+  //   border-radius: 5px;
+  //   height: auto;
+  //   width: 10rem;
+  // }
   div {
     text-align: center;
   }
