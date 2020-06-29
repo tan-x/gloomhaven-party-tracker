8dd87c69dfebfc9031ce0a9cf755217e874d724f
Tanner Griffin <tannermgriffin@gmail.com>
2020-06-29 00:04:00 -0500

    conditionally render item images and layout in groups by type

--- gh-pt/src/components/Items.js
+++ gh-pt/src/components/Items.js
@@ -1,27 +1,91 @@
 import React, { useState } from 'react';
 import stats from '../stats';
-import icon from '../assets/class-icons/07 Sun.png'
+import head from '../assets/equip-slots/head.png';
+import body from '../assets/equip-slots/body.png';
+import legs from '../assets/equip-slots/legs.png';
+import hand from '../assets/equip-slots/hand.png';
+import small from '../assets/equip-slots/small.png';
 
 export default function Items(props) {
 	const [items, setitems] = useState(stats[props.route].items);
-	var itemNames = items.map((item) => {
-		return item.name.map(name => name);
-    });
-    itemNames = itemNames.flat();
-    console.log(itemNames);
+    let headItems = [], bodyItems = [], legItems = [], handItems = [], smallItems = [];
+    
+    for (let i=0; i<items.length; i++) {
+        for (const item of items[i].name) {
+            switch (items[i].type) {
+                case 'head':
+                    headItems.push(item);
+                    break;
+                case 'body':
+                    bodyItems.push(item);
+                    break;
+                case 'leg':
+                    legItems.push(item);
+                    break;
+                case 'hand':
+                    handItems.push(item);
+                    break;
+                case 'small':
+                    smallItems.push(item);
+                    break;
+            }
+        }
+    }
+    
 	return (
 		<>
 			<h3 className='modal-header'>Items</h3>
 			<div>
-				{itemNames.map((item, key) => {
+                {headItems.length > 0 && <img src={head} className="header-class-logo"/>}
+				{headItems.map((item, key) => {
                         return (
                             <>
-                                <img src={icon} className="header-class-logo"/>
                                 <p key={key}>{item}</p>
-                                {key < itemNames.length -1 && <hr />}
+                                {key < headItems.length -1}
                             </>
                         );
                     })}
+                {headItems.length > 0 && <hr />}
+                {bodyItems.length > 0 && <img src={body} className="header-class-logo"/>}
+				{bodyItems.map((item, key) => {
+                        return (
+                            <>
+                                <p key={key}>{item}</p>
+                                {key < bodyItems.length -1}
+                            </>
+                        );
+                    })}
+                {bodyItems.length > 0 && <hr />}
+                {legItems.length > 0 && <img src={legs} className="header-class-logo"/>}
+				{legItems.map((item, key) => {
+                        return (
+                            <>
+                                <p key={key}>{item}</p>
+                                {key < legItems.length -1}
+                            </>
+                        );
+                    })}
+                {legItems.length > 0 && <hr />}
+                {handItems.length > 0 && <img src={hand} className="header-class-logo"/>}
+				{handItems.map((item, key) => {
+                        return (
+                            <>
+                                <p key={key}>{item}</p>
+                                {key < handItems.length -1}
+                            </>
+                        );
+                    })}
+                {handItems.length > 0 && <hr />}
+                {smallItems.length > 0 && <img src={small} className="header-class-logo"/>}
+				{smallItems.map((item, key) => {
+                        return (
+                            <>
+                                <p key={key}>{item}</p>
+                                {key < smallItems.length -1}
+                            </>
+                        );
+                    })}
+                {smallItems.length > 0 && <hr />}
 			</div>
 		</>
 	);
