41203f7ff2c223d14fd9d632da9b77da6a78ea27
Tanner Griffin <tannermgriffin@gmail.com>
2020-07-10 00:39:12 -0500

    Debugging unlock check issue

--- client/src/components/Unlocks.js
+++ client/src/components/Unlocks.js
@@ -5,15 +5,14 @@ import StatContext from '../Context';
 export default function Unlocks(props) {
 	const statContext = useContext(StatContext);
     const [unlockView, setUnlockView] = useState('scenario');
-
+    const unlocks = statContext[8].unlocks;
     useEffect(() => {
-        console.log(statContext[8])
         firebase.firestore().collection(statContext[4][0]).doc('items').get().then((res) => {
             statContext[9](res.data());
         })
     }, [])
 
-    const unlocks = statContext[8].unlocks;
+
 	const scenarioArray = [];
 	const treasureArray = [];
 	const eventArray = [];
@@ -23,7 +22,8 @@ export default function Unlocks(props) {
 		if (confirm) {
 			e.target.nextSibling.nextSibling.className = 'unlock-text';
 			e.target.disabled = true;
-			unlockItem(e.target.name, e.target.id);
+            unlockItem(e.target.name, e.target.id);
+            console.log(e.target.name, e.target.id)
 		} else {
 			e.target.checked = false;
 		}
@@ -33,7 +33,7 @@ export default function Unlocks(props) {
         const newItems = Object.assign({}, statContext[8]);
         // console.log(newItems);
         const itemRef = newItems.unlocks[type][item];
-        itemRef.unlocked = true;
+        newItems.unlocks[type][item].unlocked = true;
         if (itemRef.type === 'shop') {
             for (let i = (itemRef.itemNum[0] - 1); i < itemRef.itemNum[1]; i++) {
                 newItems.shop[i].available = newItems.shop[i].total;
@@ -44,19 +44,19 @@ export default function Unlocks(props) {
                 newItems.shop[itemRef.itemNum - 1].available -= 1;
             }
         }
-		statContext[9](newItems);
-		firebase
-			.firestore()
-			.collection(statContext[4][0])
-			.doc('items')
-			.update(newItems);
+		// statContext[9](newItems);
+		// firebase
+		// 	.firestore()
+		// 	.collection(statContext[4][0])
+		// 	.doc('items')
+		// 	.update(newItems);
 	};
 
 	unlocks.scenario.forEach((item, i) => {
 		if (!item.unlocked) {
 			scenarioArray.push(
 				<div className='unlock-row' key={i}>
-					<input type='checkbox' id={i} name='scenario' onChange={handleUnlock} />
+					<input type='checkbox' id={i} name='scenario' onChange={handleUnlock} defaultChecked={false}/>
 					<span className='unlock-num'>{item.scenario}. </span>
 					<p className='unlock-blur unlock-text'>{item.info}</p>
 				</div>
@@ -76,7 +76,7 @@ export default function Unlocks(props) {
 		if (!item.unlocked) {
 			treasureArray.push(
 				<div className='unlock-row' key={i}>
-					<input type='checkbox' id={i} name='treasure' onChange={handleUnlock} />
+					<input type='checkbox' id={i} name='treasure' onChange={handleUnlock} defaultChecked={false}/>
 					<span className='unlock-num'>{item.treasure}. </span>
 					<p className='unlock-blur unlock-text'>{item.info}</p>
 				</div>
@@ -84,19 +84,19 @@ export default function Unlocks(props) {
 		} else {
 			treasureArray.push(
 				<div className='unlock-row' key={i}>
-					<input type='checkbox' id={i} disabled checked />
+					<input type='checkbox' id={i} disabled checked/>
 					<span className='unlock-num'>{item.treasure}. </span>
 					<p className='unlock-text'>{item.info}</p>
 				</div>
 			);
 		}
-	});
+    });
 
 	unlocks.event.forEach((item, i) => {
 		if (!item.unlocked) {
 			eventArray.push(
 				<div className='unlock-row' key={i}>
-					<input type='checkbox' id={i} name='event' onChange={handleUnlock} />
+					<input type='checkbox' id={i} name='event' onChange={handleUnlock} defaultChecked={false}/>
 					<span className='unlock-num'>{item.event}. </span>
 					<p className='unlock-blur unlock-text'>{item.info}</p>
 				</div>
@@ -104,7 +104,7 @@ export default function Unlocks(props) {
 		} else {
 			eventArray.push(
 				<div className='unlock-row' key={i}>
-					<input type='checkbox' id={i} disabled checked />
+					<input type='checkbox' id={i} disabled checked/>
 					<span className='unlock-num'>{item.event}. </span>
 					<p className='unlock-text'>{item.info}</p>
 				</div>
