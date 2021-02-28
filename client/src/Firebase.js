eead44db6412d24981ae693c702a99997be13863
Jordy Pereira <jordy.pereira@prorenata.se>
2021-02-28 12:59:09 +0100

    Use env file for app keys

--- client/src/Firebase.js
+++ client/src/Firebase.js
@@ -3,14 +3,13 @@ import * as firebaseui from 'firebaseui';
 import 'firebase/firestore';
 
 export const firebaseConfig = {
-	apiKey: 'AIzaSyDMP0FGRfMfSIQIYc0x1qlW1SSwKA_DRf4',
-	authDomain: 'ghpartytracker.firebaseapp.com',
-	databaseURL: 'https://ghpartytracker.firebaseio.com',
-	projectId: 'ghpartytracker',
-	storageBucket: 'ghpartytracker.appspot.com',
-	messagingSenderId: '675542052221',
-	appId: '1:675542052221:web:782ae63b97562e49b93014',
-	measurementId: 'G-39WBY8QF9B',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID 
 };
 
 firebase.initializeApp(firebaseConfig);
